b3bbbde16343773e5934b3bb64e948abe219334a
LUCKYanyone <112945159+LUCKYanyone@users.noreply.github.com>
2023-09-21 17:30:08 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxdb8cf4d0cbaa3f48",
+  appsecret: "de1348cf73dbd34da9618acedd6d6cc6",
+  template_id: "TrsSjq01zcFj3mXqN7CSc-ljeeGkfmmj3RJmCMiCDiI",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "洛阳",
+  first_date: "2023-03-06",
 };
